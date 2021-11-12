d197560988cf5ac24f7f7d97315fb810eb8c6954
Ivan Tarozzi <itarozzi@gmail.com>
2021-11-12 01:44:53 +0100

    classi esterne per midi e footswitch

--- software/esp32/platformio_sources/flr_ble_midi_board/src/main.cpp
+++ software/esp32/platformio_sources/flr_ble_midi_board/src/main.cpp
@@ -40,12 +40,11 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <MidiHelper.h>
+#include <FootSwitch.h>
 #define SERVICE_UUID        "03b80e5a-ede8-4b33-a751-6ce34ec4c700"  // The MIDI Service
 #define CHARACTERISTIC_UUID "7772e5db-3868-4112-a1a9-f2669d106bf3"  // The MIDI Characteristic
 
-#define DEBOUNCE_TIME 1  //ms
-
-#define HOLD_TIME  1000  //ms
 
 
 // TODO: this vars should be moved to config
@@ -66,9 +65,7 @@ const int fs3_led_pin = 39;
 const int fs4_led_pin = 39;
 
 
-const int cc_code_btn1 = 0x20;     
-const int cc_code_btn2 = 0x21;     
-const int cc_code_btn3 = 0x22;     
+     
 
 
 #define ARRAY_LENGTH(x) (sizeof(x)/sizeof(x[0]))
@@ -77,7 +74,7 @@ typedef void (*isr_callback)();
 
 struct hardware_interrupt
 {
-  uint8_t pin_no;
+  int pin_no;
   isr_callback isr;
 };
 
@@ -93,35 +90,33 @@ struct footswitch
 };
 
 
-footswitch  footswitchArray[fs_number]; 
+
+// footswitch  footswitchArray[fs_number]; 
+FootSwitch  footswitchArray[fs_number]; 
 
 BLECharacteristic *pCharacteristic;
 int cc_code_prev = 0;
 
-void send_midi_command(bool isCC, u_char midi_channel, u_char midi_cmd_nr, u_char midi_cmd_value) {
+void send_midi_command(MidiHelper::MidiMessageType msg_type, u_char midi_channel, u_char midi_cmd_nr, u_char midi_cmd_value) {
 
   //  TODO: inserire il controllo sul range dei comandi 
 
-  
-  uint8_t midiPacket[] = {
-    0x80,  // header
-    0x80,  // timestamp, not implemented 
-    0x00,  // type + channel
-    0x3c,  // data nr - key note: 0x3c == 60 == middle c
-    0x00   // data value
-  };
+  MidiHelper midiHelper;
+  __UINT8_TYPE__ msg_tx[10];
 
-  /** Send the CC MIDI message ON **/
-  midiPacket[2] = 0xB0 + midi_channel -1; // CC on channel midi_channel
-  midiPacket[3] = midi_cmd_nr; // Control number
-  midiPacket[4] = midi_cmd_value;    // Control value
-  Serial.println("sending MIDI packet ..."); 
-  
-  pCharacteristic->setValue(midiPacket, 5); // packet, length in bytes
-  pCharacteristic->notify();
-  vTaskDelay(100/portTICK_PERIOD_MS);
+  if ( midiHelper.buildMidiCommand(msg_type, midi_channel,midi_cmd_nr, midi_cmd_value , msg_tx) ) {
 
-}
+    Serial.println("Sending MIDI message");
+    for (int i=0 ; i< 10; i++) {
+      Serial.print(msg_tx[i]);
+    }
+    
+    pCharacteristic->setValue(msg_tx, 5); // packet, length in bytes
+    pCharacteristic->notify();
+    vTaskDelay(100/portTICK_PERIOD_MS);
+  }
+
+ }
 
 
 void process_tap_event(uint8_t fs_nr) {
@@ -129,7 +124,7 @@ void process_tap_event(uint8_t fs_nr) {
 
   // TODO: gestire invio MIDI sulla base della configurazione
 
-  send_midi_command(true, 1 , 12, 1);
+  send_midi_command(MidiHelper::MIDI_CC, 1 , 12, 1);
 
 }
 
@@ -138,37 +133,35 @@ void process_hold_event(uint8_t fs_nr) {
 
   Serial.println("Processing HOLD Event   " + fs_nr); 
 
-  send_midi_command(true, 1, 18, 5);
+  send_midi_command(MidiHelper::MIDI_CC, 1, 18, 5);
 }
 
 
 void process_interrupt(uint8_t fs_nr, bool new_state_pressed) {
-  footswitch *my_footswitch = &footswitchArray[fs_nr-1];
+  //footswitch *my_footswitch = &footswitchArray[fs_nr-1];
 
+  FootSwitch *my_footswitch = &footswitchArray[fs_nr-1];
   // Serial.print("Processing Interrupt "); 
   // Serial.print(fs_nr);
   // Serial.println(new_state_pressed?"P":"R"); 
 
  
 
-  if ((new_state_pressed)  && (!my_footswitch->pressed)) {
-    if ((millis() - my_footswitch->t_released) > DEBOUNCE_TIME) {
-      my_footswitch->t_pressed = millis();
-      my_footswitch->pressed = true;
-
+  if ((new_state_pressed)  && (!my_footswitch->isPressed())) {
+    if  (my_footswitch->press(millis())) {
       Serial.println("PRESSED "); 
     }
   }
-  else if ((!new_state_pressed)  && (my_footswitch->pressed)) {
-    if ((millis() - my_footswitch->t_pressed) > DEBOUNCE_TIME) {
-      my_footswitch->t_released = millis();
-      my_footswitch->pressed = false;
-      my_footswitch->hold = false;
-      Serial.println("RELEASED "); 
+  else if ((!new_state_pressed)  && (my_footswitch->isPressed())) {
+    bool tap_event = false;
 
-      if ( (my_footswitch->t_released - my_footswitch->t_pressed) < HOLD_TIME) {
-        process_tap_event(fs_nr);
-      }
+    if (my_footswitch->release(millis(), &tap_event)) {
+      Serial.println("RELEASED ");
+    }
+
+    if (tap_event) {
+      Serial.println("TAP EVENT");
+      process_tap_event(fs_nr);
     }
   }
 }
@@ -235,52 +228,18 @@ void setup() {
 }
 
 
-
 void loop() {
 
 
   for (uint8_t idx = 0; idx<fs_number; idx++) {
-    if ((footswitchArray[idx].pressed) && 
-        (!footswitchArray[idx].hold) && 
-        ((millis() - footswitchArray[idx].t_pressed) >= HOLD_TIME )
-       ) {
-         footswitchArray[idx].hold = true;
-         process_hold_event(idx+1);
+
+    if (footswitchArray[idx].checkHold(millis())) {
+      process_hold_event(idx+1);
     }
   }
   
   //TODO: gestire il caso di HOLD_REPEAT !!!
 
- 
   
-  
-  // if ((cc_code > 0) && (cc_code != cc_code_prev)) {
-  //   /** Send the CC MIDI message ON **/
-  //   midiPacket[2] = 0xB0 + midi_channel -1; // CC on channel midi_channel
-  //   midiPacket[3] = cc_code; // Control number
-  //   midiPacket[4] = 0x01;    // Control value
-  //   Serial.println("sending MIDI packet ..."); 
-    
-  //   pCharacteristic->setValue(midiPacket, 5); // packet, length in bytes
-  //   pCharacteristic->notify();
-  //   vTaskDelay(100/portTICK_PERIOD_MS);
- 
-  // }
-  // if ((cc_code == 0) && ( cc_code_prev>0)) {
-    
-  //   /** Send the CC MIDI message OFF **/
-  //   midiPacket[2] = 0xB0 + midi_channel - 1; // CC on channel midi_channel
-  //   midiPacket[3] = cc_code_prev; // Control number
-  //   midiPacket[4] = 0x00;         // Control value
-  //   Serial.println("sending MIDI packet ..."); 
-    
-  //   pCharacteristic->setValue(midiPacket, 5); // packet, length in bytes
-  //   pCharacteristic->notify();
-  //   vTaskDelay(100/portTICK_PERIOD_MS);
-
-  // }
-  // cc_code_prev = cc_code;
-
-
   delay(50);
 }
